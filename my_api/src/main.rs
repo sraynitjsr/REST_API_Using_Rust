3068339cc0a7179c7c7e9c66cf3fa6f9fceaf64d
unknown <subhradeepray2017@gmail.com>
2023-07-20 00:21:23 +0530

    Created PUT Endpoint

--- my_api/src/main.rs
+++ my_api/src/main.rs
@@ -1,7 +1,8 @@
 #[macro_use]
 extern crate rocket;
 
-use rocket::serde::json::Json;
+use rocket::serde::json::{Json, Value};
+use rocket::State;
 
 #[derive(Debug, Serialize, Deserialize)]
 struct User {
@@ -10,30 +11,61 @@ struct User {
     email: String,
 }
 
+#[derive(Debug, Serialize, Deserialize)]
+struct UpdateUser {
+    name: Option<String>,
+    email: Option<String>,
+}
+
+struct AppState {
+    users: Vec<User>,
+}
+
 #[get("/")]
 fn index() -> &'static str {
     "Hello, World!"
 }
 
 #[get("/users")]
-fn get_users() -> Json<Vec<User>> {
-    let users = vec![
-        User {
-            id: 1,
-            name: String::from("A"),
-            email: String::from("A1#@example.com"),
-        },
-        User {
-            id: 2,
-            name: String::from("B"),
-            email: String::from("B1^@example.com"),
-        },
-    ];
+fn get_users(state: &State<AppState>) -> Json<Vec<User>> {
+    Json(state.users.clone())
+}
 
-    Json(users)
+#[put("/users/<id>", data = "<update>")]
+fn update_user(id: u32, update: Json<UpdateUser>, state: &State<AppState>) -> Option<Json<User>> {
+    let mut users = state.users.clone();
+
+    for user in &mut users {
+        if user.id == id {
+            if let Some(name) = &update.name {
+                user.name = name.clone();
+            }
+            if let Some(email) = &update.email {
+                user.email = email.clone();
+            }
+            return Some(Json(user.clone()));
+        }
+    }
+
+    None
 }
 
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/", routes![index, get_users])
+    let users = vec![
+        User {
+            id: 1,
+            name: String::from("A"),
+            email: String::from("A$1@example.com"),
+        },
+        User {
+            id: 2,
+            name: String::from("B"),
+            email: String::from("B2%@example.com"),
+        },
+    ];
+
+    rocket::build()
+        .manage(AppState { users })
+        .mount("/", routes![index, get_users, update_user])
 }
