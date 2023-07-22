4a82a551f138d7afc8f7587fb394406c58807db4
unknown <subhradeepray2017@gmail.com>
2023-07-22 09:32:51 +0530

    CRUD Using RUST Rocket

--- my_api/src/main.rs
+++ my_api/src/main.rs
@@ -3,5 +3,12 @@ extern crate rocket;
 mod controller;
 
 fn main() {
-    rocket::ignite().mount("/", routes![controller::hello, controller::get_user]).launch();
+    rocket::ignite().mount("/", routes![
+        controller::hello,
+        controller::UserController::get_all_users,
+        controller::UserController::get_user,
+        controller::UserController::add_user,
+        controller::UserController::update_user,
+        controller::UserController::delete_user,
+    ]).launch();
 }
