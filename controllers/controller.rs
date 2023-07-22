779404618348308391d88d7a7ae2f6740d78af3c
unknown <subhradeepray2017@gmail.com>
2023-07-22 09:25:21 +0530

    Get User By Id Controller

--- controllers/controller.rs
+++ controllers/controller.rs
@@ -1,6 +1,33 @@
 use rocket::get;
+use rocket::response::content::Json;
 
-#[get("/")]
-pub fn hello() -> &'static str {
-    "Hello, World!"
+struct User {
+    id: u32,
+    name: String,
+    email: String,
+}
+
+fn get_user_by_id(user_id: u32) -> Option<User> {
+    let users = vec![
+        User {
+            id: 1,
+            name: "A".to_string(),
+            email: "A1@gmail.com".to_string(),
+        },
+        User {
+            id: 2,
+            name: "B".to_string(),
+            email: "B2@gmail.com".to_string(),
+        },
+    ];
+
+    users.into_iter().find(|user| user.id == user_id)
+}
+
+pub fn get_user(id: u32) -> Option<Json<User>> {
+    if let Some(user) = get_user_by_id(id) {
+        Some(Json(user))
+    } else {
+        None
+    }
 }
